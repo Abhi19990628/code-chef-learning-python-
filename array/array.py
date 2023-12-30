c6d2dd4f219563219d13c58a219e540402f127ad
abhihek kumar <abhbiv5976@gmail.com>
2023-12-30 21:08:41 +0530

    main

--- array/array.py
+++ array/array.py
@@ -27,3 +27,115 @@ if __name__ == '__main__':
 
 	# Thanks to Aditi Sharma for contributing
 	# this code
+
+
+#   next qution   $$$$$ -----------
+
+
+
+# Write a Python program to create an array of 5 integers and display the array items.
+# Access individual elements through indexes.
+
+from array import *
+array_num = array('i', [1,3,5,7,9])
+for i in array_num:
+    print(i)
+print("Access first three items individually")
+print(array_num[0])
+print(array_num[1])
+print(array_num[2])
+
+
+
+
+# access any num from anywhere if you want 
+
+
+from array import *
+array_num = array('i', [1,2,3,4,5])
+
+for i in array_num:
+  print(i)
+
+print("access the first three items")
+
+print(array_num [0])
+print(array_num [4])
+print(array_num [3])
+
+# append program in array
+
+ 
+list = [1,2,3,4,5]
+x= [6,7,8]
+list.append(x)
+print(list)
+
+# secound program in append 
+
+from array import *
+array_num = array('i', [1, 3, 5, 7, 9])
+print("Original array: "+str(array_num))
+print("Append 11 at the end of the array:")
+array_num.append(11)
+print("New array: "+str(array_num))
+
+
+
+import numpy as np
+
+x=np.arange(3)
+print("Array x : ", x)
+
+y=np.arange(10,15)
+print("Array y : " ,y)
+
+result= np.append(x,y)
+print("result after appending x and y:",result)
+
+
+# simple caculater
+
+a=50
+b=3
+
+print( "the value of", a , " + " , b , "is:" , a+b)
+print( "the value of", a , " * " , b , "is:" , a*b)
+print( "the value of", a , " - " , b , "is:" , a-b)
+print( "the value of", a , " / " , b , "is:" , a/b)
+
+# type casting
+
+a="1" # one is string becouse we use this ("")
+b="3" # 3 num is also string 
+#print (a+b)
+
+# we got the result  13
+
+# if want 1+3=4
+
+#a=1
+#b=3
+#print(a+b)
+
+# so we use type of casting 
+print(int(a)+int (b))
+#first tell computer int is value 
+
+string="5"
+number = 3
+string_number=int (string)
+sum = string_number+number
+print (sum)
+
+# input method
+
+a=input("Enter your first name: ")
+b=input("Enter your lastname: ")
+print("my name is : ", a+b )
+
+# on math
+
+X = input("Enter your first num: ")
+y = input("Enter your second num : ")
+print(x + y)
---
+++ array/main.py
@@ -0,0 +1,17 @@
+# input as intnger
+
+#and how get input 
+
+# x=input("enter fst num : ")
+# y=input("enter sec num : ")
+# print(x+y)
+# print(int(x)+int(y))
+
+# str='''Klmlm Lmo is on Facebook. Join Facebook to connect with Klmlm Lmo and others you may know. Facebook gives people the power to share and makes the world.'''
+# print(str)
+
+
+a = "abhishek"
+print(len(a))
+# print(a.upper())
+# print(a.replace ("abhishek","ramnath"))
