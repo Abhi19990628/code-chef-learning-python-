c0bb8c10a538d33b83e9cc4d4f79ce8157ea0b52
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-02-17 22:22:16 +0530

    Update variables.py

--- Variables in Python/variables.py
+++ Variables in Python/variables.py
@@ -149,6 +149,33 @@ print(a,b)
 
 #Next up is a small quiz on what we have learned till now.
 
+        self.item = item
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+
+if __name__ == '__main__':
+
+    linked_list = LinkedList()
+
+    # Assign item values
+    linked_list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    # Connect nodes
+    linked_list.head.next = second
+    second.next = third
+
+    # Print the linked list item
+    while linked_list.head != None:
+        print(linked_list.head.item, end=" ")
+        linked_list.head = linked_list.head.next
 #Then we are going to learn about another data type in the next lesson and solve slightly harder problems to deeply understand how Python works.
 
 #This knowledge will make you a master in Python real soon. Don't get discouraged when you get Wrong answer or Runtime error. Everybody makes mistakes while learning something.
